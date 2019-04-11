ced0c15281b5e183ac4f220b69ff62a0f5c8600e
Cassidy James Blaede <c@ssidyjam.es>
2019-04-11 15:32:03 -0600

    Tweak styles

--- store/index.php
+++ store/index.php
@@ -73,14 +73,14 @@
         <h4>Dell XPS 13</h4>
         <p>Incredibly thin and light. 13&Prime; HiDPI display. Aluminum and carbon fiber.</p>
         <p data-l10n-off="1" class="text-center">$949+</p>
-        <a href="https://www.dell.com/en-us/work/shop/dell-laptops-and-notebooks/xps-13-laptop/spd/xps-13-9380-laptop" target="_blank" class="button">Buy from Dell</a>
+        <a href="https://www.dell.com/en-us/work/shop/dell-laptops-and-notebooks/xps-13-laptop/spd/xps-13-9380-laptop" target="_blank" class="button suggested-action">Buy from Dell</a>
     </div>
     <div class="grid__item">
         <img src="/images/store/devices/dell-precision-5530.png"/>
-        <h4>Dell Precision</h4>
+        <h4>Dell Precision 5530</h4>
         <p>Sleek professional workstation. 15&Prime; HiDPI display. Aluminum and carbon fiber.</p>
         <p data-l10n-off="1" class="text-center">$1,169+</p>
-        <a href="https://www.dell.com/en-us/work/shop/workstations-isv-certified-dell/precision-5530/spd/precision-15-5530-laptop" target="_blank" class="button">Buy from Dell</a>
+        <a href="https://www.dell.com/en-us/work/shop/workstations-isv-certified-dell/precision-5530/spd/precision-15-5530-laptop" target="_blank" class="button suggested-action">Buy from Dell</a>
     </div>
 </div>
 
